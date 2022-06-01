a023d6c497a49cf1982a5e001147321fd43678bc
lenght1 <11025869+zhangweijie1-a@user.noreply.gitee.com>
2022-06-01 14:55:52 +0800

    6/1

--- js/register.js
+++ js/register.js
@@ -1,47 +1,47 @@
-class register{
+class Register {
+
     constructor() {
-        // 给属性赋值,调用其它方法
-       
-        
-        
-        
-      }
-      binkEve(){
-        this.$('.over').addEventListener('click', this.getData)
-      }
-     getData(){
-        let uN=this.$('.tel').value;
-        let nN=this.$('.code').value;
-        let uP=this.$('.psw').value;
-        let uP1=this.$('.repsw').value;
-        let zz= /\w{4,20}/;
-        var regnc = /^[u4e00-u9fa5]{2,8}$/; 
-        if(!uN||!nN||!uP||!uP1){
-            alert("输入框不能为空！")
-
-        }
-
-        let xhr=new XMLHttpRequest();
-        xhr.open('post','http://localhost:8888/users/register');
-        xhr.setRequestHeader('constent-type','application/x-www-form-urlencoded');
-        let data=(`username=${uN}&password=${nP}&rpassword=${nP}&nickname=${nN}`)
-        if(!uN.match(zz)||!uP.match(zz)||!uP==uP1||!nN.match(regnc)){
-            // xhr.send(data)
-            alert("输入不符合要求");
-            return;
-        }else{
-            xhr.send(data);
-            console.log(111);
-            xhr.onload=function(){
-                let res=xhr.response;
-                console.log(res);
-            }
-        }
-       
+        this.getUser();
     }
-    $(ele) {
-        let res = document.querySelectorAll(ele);
-        return res.length == 1 ? res[0] : res;
-      }
+
+    getUser() {
+        Register.$('.over').addEventListener('click', this.regiFn);
+    }
+
+    regiFn = (eve) => {
+        eve.preventDefault();
+        let form = document.forms[0];
+        let username = form.tel.value;
+        // console.log(username);
+        let password = form.password.value;
+        // console.log(form.mm);
+        let rpassword = form.repassword.value;
+        // console.log(rpassword);
+        let nickname = form.code.value;
+        // console.log(nickname);
+        if (!username || !password || !rpassword || !nickname) {
+            alert('不能为空')
+        }
+        let data = `username=${username}&password=${password}&rpassword=${rpassword}&nickname=${nickname}`;
+        axios.post('http://localhost:8888/users/register/', data).then(res => {
+            console.log(res);
+            let { status, data } = res
+            if (status == 200 && data.code == 1) {
+                location.href = './login.html';
+            } else {
+                let str = data.message;
+                Register.$('.reg_form .noLogo div').innerHTML = str;
+                Register.$('.reg_form .noLogo').style.display = 'block'
+            }
+        })
+    }
+
+
+
+    static $(tag) {
+        let tagArr = document.querySelectorAll(tag);
+        return tagArr.length == 1 ? tagArr[0] : tagArr;
+    }
 }
-new  register();
+
+new Register;
